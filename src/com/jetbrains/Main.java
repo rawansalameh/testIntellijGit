739841708d5896ddec4a7d9e00462a4d48acafb6
Rawan <rawansalameh160596@gmail.com>
2020-04-23 19:35:59 +0300

    test

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,19 @@
+package com.jetbrains;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        UniversityCourse Java = new UniversityCourse("JAVA", "Dr. Fahed");
+        UniversityCourse DevOps = new UniversityCourse("DevOps", "Dr. Motasem");
+        Java.register("Rawan Salameh");
+    }
+
+       }
+
+
---
+++ src/com/jetbrains/UniversityCourse.java
@@ -0,0 +1,24 @@
+package com.jetbrains;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class UniversityCourse {
+    private String courseName;
+    private String instructorName;
+    private ArrayList<String> students = new ArrayList<>();
+
+    public UniversityCourse(String courseName, String instructorName) {
+        this.courseName = courseName;
+        this.instructorName = instructorName;
+    }
+
+    public void register(String studentname) {
+        if (studentname == null) {
+            throw new IllegalArgumentException();
+        } else {
+            students.add(studentname);
+        }
+    }
+}
+
